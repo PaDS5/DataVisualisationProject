61734f3cad79f365456c00dfd92a877e71ffb2f8
DORFFER_Paul <paul.dorffer@orange.fr>
2021-03-13 20:23:22 +0100

    Ajout graphes

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import dash as dash
+import geojson as geojson
 from dash import Dash
 import plotly.express as px
 import dash_core_components as dcc
@@ -36,6 +37,13 @@ df_new_filtered = df[df['sexe'] == 0].groupby('dep')
 
 fig = px.pie(df_filtered, values="rea", names="sexe")
 
+with open('departements.geojson') as file:
+    geo = geojson.load(file)
+
+figtest = px.choropleth(df, geojson= geo, locations='dep', featureidkey="properties.code", color_continuous_scale="Viridis", scope="europe")
+figtest.update_geos(showcountries=False, showcoastlines=False, showland=False, fitbounds="locations")
+#figtest.update_layout(margin={"r":50,"t":50,"l":50,"b":50})
+figtest.update_layout(width = 1000, height = 1000)
 
 fig_bot_right = px.pie(df_filtered, values="hosp", names="sexe_str", title = "Hospitalisations by Gender")
 fig_bot_right.update_traces(textposition='outside',
@@ -49,8 +57,8 @@ fig_bot_right.update_traces(textposition='outside',
 
 fig_bot_left = px.pie(df_filtered, values="rea", names="sexe_str", title = "Reanimation by Gender")
 
-fig_left = px.scatter(df_cumul_hosp, x ='jour', y='cumul_hosp', title="Evolution of the number of people hospitalized in France")
-fig_rr = px.scatter(df_cumul_hosp, x ='jour', y='cumul_rea', title="Evolution of the number of people in reanimation in France")
+fig_total_hosp = px.scatter(df_cumul_hosp, x ='jour', y='cumul_hosp', title="Evolution of the number of people hospitalized in France")
+fig_total_rea = px.scatter(df_cumul_hosp, x ='jour', y='cumul_rea', title="Evolution of the number of people in reanimation in France")
 
 app.layout = html.Div([
     html.H1('Covid Data in France', style={"textAlign": "center"}),
@@ -77,21 +85,93 @@ app.layout = html.Div([
     ], className= 'row', style={"backgroundColor": "#313131"}),
 
     html.Br(),
+    html.Br(),
 
     html.Div([
 
         html.Div([
             dcc.Graph(
                 id='graph1',
-                figure=fig_left
+                figure=fig_total_hosp
             ),
 
             html.Br(),
 
             dcc.Graph(
                 id='graph4',
-                figure=fig_rr),
-        ], className='six columns', style={"height":"100%", "vertical-align":"center"}),
+                figure=fig_total_rea),
+        ], className='six columns'),
+
+        html.Div([
+            html.Br()
+        ], className="one columns"),
+
+        html.Div([
+            html.H4('Second paragraph', style={"color": "white", "textAlign": "center"}),
+            html.Br(),
+            html.P("Now let's have a look at the total number of people who were hospitalized and who were in reanimation",
+                   style={"textAlign": "justify", "color": "white"}),
+            html.Br(),
+            html.P(
+                'We can see the number of hospitalisations every day, number of reanimation and deaths (cumulated)\n',
+                style={"textAlign": "justify", "color": "white"}),
+            html.Br(),
+
+            html.P("Now let's have a look at the total number of people who were hospitalized and who were in reanimation",
+                   style={"textAlign": "justify", "color": "white"}),
+            html.Br(),
+            html.P("Now let's have a look at the total number of people who were hospitalized and who were in reanimation",
+                   style={"textAlign": "justify", "color": "white"}),
+            html.Br(),
+            html.P("Now let's have a look at the total number of people who were hospitalized and who were in reanimation",
+                   style={"textAlign": "justify", "color": "white"}),
+            html.Br(),
+            html.P("Now let's have a look at the total number of people who were hospitalized and who were in reanimation",
+                   style={"textAlign": "justify", "color": "white"}),
+            html.Br(),
+            html.P("Now let's have a look at the total number of people who were hospitalized and who were in reanimation",
+                   style={"textAlign": "justify", "color": "white"}),
+            html.Br(),
+            html.P("Now let's have a look at the total number of people who were hospitalized and who were in reanimation",
+                   style={"textAlign": "justify", "color": "white"}),
+            html.Br(),
+            html.P("Now let's have a look at the total number of people who were hospitalized and who were in reanimation",
+                   style={"textAlign": "justify", "color": "white"}),
+            html.Br(),
+            html.P("Now let's have a look at the total number of people who were hospitalized and who were in reanimation",
+                   style={"textAlign": "justify", "color": "white"}),
+            html.Br(),
+            html.P("Now let's have a look at the total number of people who were hospitalized and who were in reanimation",
+                   style={"textAlign": "justify", "color": "white"}),
+            html.Br(),
+            html.P("Now let's have a look at the total number of people who were hospitalized and who were in reanimation",
+                   style={"textAlign": "justify", "color": "white"}),
+            html.Br(),
+
+
+        ], className="six columns", style={"backgroundColor": "#313131","padding": "3%"}),
+
+
+        #dcc.Graph(id = 'third-graph', figure= fig_bot_right, className='three columns'),
+        #dcc.Graph(id = 'fourth-graph', figure= fig_bot_right, className='three columns')
+    ],className="row", style={"border": "solid", "padding":"2%"}),
+
+    html.Div([
+
+
+        html.Div([
+            html.H4('Second paragraph', style={"color": "white", "textAlign": "center"}),
+            html.Br(),
+            html.P(
+                "Now let's have a look at the total number of people who were hospitalized and who were in reanimation",
+                style={"textAlign": "justify", "color": "white"}),
+            html.Br(),
+            html.P(
+                'We can see the number of hospitalisations every day, number of reanimation and deaths (cumulated)\n',
+                style={"textAlign": "justify", "color": "white"}),
+            html.Br(),
+
+        ], className="seven columns", style={"backgroundColor": "#313131", "padding": "3%"}),
 
         html.Div([
             html.Br()
@@ -110,11 +190,31 @@ app.layout = html.Div([
         ], className="four columns"),
 
 
-        #dcc.Graph(id = 'third-graph', figure= fig_bot_right, className='three columns'),
-        #dcc.Graph(id = 'fourth-graph', figure= fig_bot_right, className='three columns')
-    ],className="row", style={"backgroundColor": "#800081", "border": "solid"})
+    ], className="row", style={"padding": "3%", "border": "solid"}),
 
+    html.Div([
 
+        html.Div([
+            dcc.Graph(
+                id='graph8',
+                figure=figtest),
+        ], className="seven columns", style={"height":"100%"}),
+
+        html.Div([
+            html.H4('Second paragraph', style={"color": "white", "textAlign": "center"}),
+            html.Br(),
+            html.P(
+                "Now let's have a look at the total number of people who were hospitalized and who were in reanimation",
+                style={"textAlign": "justify", "color": "white"}),
+            html.Br(),
+            html.P(
+                'We can see the number of hospitalisations every day, number of reanimation and deaths (cumulated)\n',
+                style={"textAlign": "justify", "color": "white"}),
+            html.Br(),
+
+        ], className="five columns", style={"backgroundColor": "#313131"}),
+
+    ])
 
 ], style={"padding": "3%", "border": "solid"})
 
@@ -130,5 +230,4 @@ def update_figure(yaxis_column_name):
 
 
 if __name__ == '__main__':
-    # app4.run_server(mode = 'inline')
     app.run_server(debug=False)
